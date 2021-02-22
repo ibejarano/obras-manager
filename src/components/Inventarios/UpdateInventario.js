f30746d2ff5ee82d719dd106b24769497850f14c
Ignacio Bejarano <ignbejarano@gmail.com>
2021-02-22 12:49:22 -0300

    implemented adding more material types

--- src/components/Inventarios/UpdateInventario.js
+++ src/components/Inventarios/UpdateInventario.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql, useMutation } from "@apollo/client";
 
+const TIPOS_PERFIL = ["L", "U", "T", "dobleT"];
 const AVAILABLE_DIAMETERS = [
   "1",
   "1 1/4",
@@ -15,7 +16,7 @@ const AVAILABLE_DIAMETERS = [
   "30",
 ];
 
-const CREATE_MATERIAL_ENTRY = gql`
+const CREATE_PIPING_ENTRY = gql`
   mutation(
     $diametro_pulg: String!
     $material: String!
@@ -42,15 +43,143 @@ const CREATE_MATERIAL_ENTRY = gql`
   }
 `;
 
+const CREATE_WELDING_ENTRY = gql`
+  mutation(
+    $descripcion: String!
+    $diametro_pulg: String!
+    $material: String!
+    $num_serie: String!
+    $cantidad: String!
+    $id: ID!
+  ) {
+    createMaterial(
+      input: {
+        data: {
+          num_serie: $num_serie
+          diametro_pulg: $diametro_pulg
+          cantidad: $cantidad
+          material: $material
+          inventario: $id
+          descripcion: $descripcion
+          tipo: welding
+        }
+      }
+    ) {
+      material {
+        num_serie
+      }
+    }
+  }
+`;
+
+const CREATE_ESTRUCTURAL_ENTRY = gql`
+  mutation(
+    $material: String!
+    $num_serie: String!
+    $cantidad: String!
+    $tipo_perfil: ENUM_MATERIAL_TIPO_PERFIL!
+    $id: ID!
+  ) {
+    createMaterial(
+      input: {
+        data: {
+          tipo_perfil: $tipo_perfil
+          num_serie: $num_serie
+          cantidad: $cantidad
+          material: $material
+          inventario: $id
+          tipo: estructural
+        }
+      }
+    ) {
+      material {
+        num_serie
+      }
+    }
+  }
+`;
+
+function SelectField({ newItem, handleChange }) {
+  const { mat_type } = newItem;
+  const optionsList =
+    mat_type == "estructural" ? TIPOS_PERFIL : AVAILABLE_DIAMETERS;
+  const labelText =
+    mat_type == "estructural" ? "Tipo de perfil" : "Diametro [pulg]";
+  const field = mat_type == "estructural" ? "tipo_perfil" : "diametro_pulg";
+
+  return (
+    <div className="col-span-6 sm:col-span-6 lg:col-span-6">
+      <label
+        htmlFor={field}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {labelText}
+      </label>
+      <select
+        name={field}
+        className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        onChange={handleChange}
+        value={newItem[field]}
+      >
+        <option selected disabled value="">
+          -Seleccione uno-
+        </option>
+        {optionsList.map((d) => (
+          <option key={d}>{d}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+function RadioButtons({ selected, handleCheck }) {
+  return (
+    <div className="col-span-6 sm:col-span-6 lg:col-span-6">
+      <input
+        type="radio"
+        id="piping"
+        name="mat_type"
+        value="piping"
+        checked={"piping" == selected}
+        onChange={handleCheck}
+      />
+      <label htmlFor="mat_type">Piping</label>
+      <input
+        type="radio"
+        id="welding"
+        name="mat_type"
+        value="welding"
+        onChange={handleCheck}
+        checked={"welding" == selected}
+      />
+      <label htmlFor="mat_type">Welding</label>
+      <input
+        type="radio"
+        id="estructural"
+        name="mat_type"
+        value="estructural"
+        onChange={handleCheck}
+        checked={"estructural" == selected}
+      />
+      <label htmlFor="mat_type">Estructural</label>
+    </div>
+  );
+}
+
 export default function AddPipingMaterial({ setOpen, idInventario, refetch }) {
   const [newItem, setNewItem] = React.useState({
+    descripcion: "",
     diametro_pulg: "8",
     cantidad: "34.341",
     material: "api60xl",
     num_serie: "lls918",
+    mat_type: "piping",
+    tipo_perfil: "",
   });
 
-  const [createMaterial] = useMutation(CREATE_MATERIAL_ENTRY);
+  const [createPiping] = useMutation(CREATE_PIPING_ENTRY);
+  const [createWelding] = useMutation(CREATE_WELDING_ENTRY);
+  const [createEstructural] = useMutation(CREATE_ESTRUCTURAL_ENTRY);
 
   const handleChange = (e) => {
     const attr = e.target.name;
@@ -59,11 +188,42 @@ export default function AddPipingMaterial({ setOpen, idInventario, refetch }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    createMaterial({
-      variables: { ...newItem, id: idInventario },
-    })
-      .then(({ data: { updateInventario } }) => refetch())
-      .catch((err) => console.log(err));
+    const outputData = { ...newItem };
+    switch (newItem.mat_type) {
+      case "piping":
+        delete outputData.descripcion;
+        delete outputData.tipo_perfil;
+        delete outputData.mat_type;
+        createPiping({
+          variables: { ...outputData, id: idInventario },
+        })
+          .then(({ data: { updateInventario } }) => refetch())
+          .catch((err) => console.log(err));
+
+        break;
+      case "welding":
+        delete outputData.tipo_perfil;
+        delete outputData.mat_type;
+        createWelding({
+          variables: { ...outputData, id: idInventario },
+        })
+          .then(({ data: { updateInventario } }) => refetch())
+          .catch((err) => console.log(err.toString()));
+        break;
+      case "estructural":
+        delete outputData.descripcion;
+        delete outputData.diametro_pulg;
+        delete outputData.mat_type;
+
+        createEstructural({
+          variables: { ...outputData, id: idInventario },
+        })
+          .then(({ data: { updateInventario } }) => refetch())
+          .catch((err) => console.log(err));
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -73,33 +233,33 @@ export default function AddPipingMaterial({ setOpen, idInventario, refetch }) {
     >
       <div className="px-4 py-5 bg-white sm:p-6">
         <div className="grid grid-cols-6 gap-6">
-          <div className="col-span-6 sm:col-span-6 lg:col-span-6">
-            <label
-              htmlFor="diametro_pulg"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Diametro [pulg]
-            </label>
-            <select
-              name="diametro_pulg"
-              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              onChange={handleChange}
-              value={newItem.diametro_pulg}
-            >
-              <option selected disabled value="">
-                -Seleccione uno-
-              </option>
-              {AVAILABLE_DIAMETERS.map((d) => (
-                <option key={d}>{d}</option>
-              ))}
-            </select>
-          </div>
+          <RadioButtons
+            handleCheck={handleChange}
+            selected={newItem.mat_type}
+          />
           <div className="col-span-6 sm:col-span-3 lg:col-span-6">
             <label
               htmlFor="cantidad"
               className="block text-sm font-medium text-gray-700"
             >
-              Cantidad [metros]
+              Descripcion (Solo para welding)
+            </label>
+            <input
+              type="text"
+              onChange={handleChange}
+              name="descripcion"
+              value={newItem.descripcion}
+              className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+              disabled={newItem.mat_type != "welding"}
+            />
+          </div>
+          <SelectField newItem={newItem} handleChange={handleChange} />
+          <div className="col-span-6 sm:col-span-3 lg:col-span-6">
+            <label
+              htmlFor="cantidad"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Cantidad (metros o unidades)
             </label>
             <input
               type="number"
