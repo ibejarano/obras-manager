1fce4349bccbf3e4c121838287d1f3f11b5823c9
Ignacio Bejarano <ignbejarano@gmail.com>
2021-03-24 11:44:33 -0300

    upload form styled

--- src/components/common/UploadForm.js
+++ src/components/common/UploadForm.js
@@ -5,6 +5,19 @@ import { toast } from "react-toastify";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf, faFolderOpen } from "@fortawesome/free-solid-svg-icons";
 
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Radio,
+  RadioGroup,
+  Stack,
+  Text,
+  Select,
+  Box,
+} from "@chakra-ui/react";
+
 const UPLOAD_FILE = gql`
   mutation($file: Upload!, $text: String!) {
     upload(file: $file, info: { caption: $text }) {
@@ -12,6 +25,30 @@ const UPLOAD_FILE = gql`
     }
   }
 `;
+
+function RenderFilesAdded({ files }) {
+  console.log(files);
+  if (files.length > 0) {
+    const renderArr = [];
+    for (let f of files) {
+      renderArr.push(
+        <Box borderBottomWidth={3} borderColor="teal.200"  >
+          <Text>
+            {f.name} <br /> Tamaño:
+            {(f.size / 1024).toFixed(1)} kB
+          </Text>
+        </Box>
+      );
+    }
+    return <>{renderArr}</>;
+  } else {
+    return (
+      <Box bg="teal.100" p={2}>
+        <Text>Agregar Archivos...</Text>
+      </Box>
+    );
+  }
+}
 
 export default function UploadCalidad({
   valTypes,
@@ -23,7 +60,7 @@ export default function UploadCalidad({
   const [name, setName] = React.useState("");
   const [fileType, setFileType] = React.useState(valTypes[0]);
   const [selectedFile, setSelectedFile] = React.useState("");
-  const [fileInfo, setFileInfo] = React.useState(null);
+  const [files, setFiles] = React.useState([]);
   const [uploadFile, { loading, error }] = useMutation(UPLOAD_FILE);
 
   const handleChange = (e) => {
@@ -52,7 +89,7 @@ export default function UploadCalidad({
       });
       toast.success("Archivos subidos satisfactoriamente.");
       setName("");
-      setSelectedFile("");
+      setSelectedFile([]);
       refetch();
     } catch {
       toast.error("ERROR: No se subieron los archivos");
@@ -60,114 +97,62 @@ export default function UploadCalidad({
   };
 
   const handleFileUpload = (e) => {
-    setFileInfo({
-      name: e.target.files[0].name,
-      size: e.target.files[0].size,
-    });
-    setSelectedFile(e.target.files[0]);
-    e.target.value = null;
+    setFiles(e.target.files);
+    setSelectedFile(e.target.files);
+    // e.target.value = null;
   };
 
   if (error) return <div>{JSON.stringify(error)}</div>;
 
   return (
-    <div className="mt-5 md:mt-0 md:col-span-2">
-      <form onSubmit={handleSubmit}>
-        <div className="shadow sm:rounded-md sm:overflow-hidden">
-          <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-            <div className="grid grid-cols-3 gap-6">
-              <div className="col-span-3 sm:col-span-2">
-                <label
-                  htmlFor="nombre_archivo"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Nombre del archivo
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                  <input
-                    type="text"
-                    name="nombre"
-                    id="nombre_archivo"
-                    className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                    placeholder="Ingrese nombre del archivo"
-                    value={name}
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-span-3 sm:col-span-2">
-                <label
-                  htmlFor="tipo_archivo"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Nombre del archivo
-                </label>
-                <div className="mt-1 flex rounded-md shadow-sm">
-                  <select onChange={(e) => setFileType(e.target.value)}>
-                    {valTypes.map((val) => (
-                      <option value={val} key={val}>
-                        {val}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-              </div>
-            </div>
+    <form onSubmit={handleSubmit}>
+      <FormControl id="nombre_archivo" my={4}>
+        <FormLabel>Nombre del archivo</FormLabel>
+        <Input
+          type="text"
+          onChange={handleChange}
+          name="nombre"
+          id="nombre_archivo"
+          value={name}
+          required
+        />
+      </FormControl>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Archivo
-              </label>
-              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div className="space-y-1 text-center">
-                  {!fileInfo && (
-                    <FontAwesomeIcon
-                      size="3x"
-                      icon={faFolderOpen}
-                      transform="shrink-6 left-4"
-                    />
-                  )}
-                  {fileInfo && (
-                    <div>
-                      <FontAwesomeIcon size="3x" icon={faFilePdf} />
-                      <p className="text-sm text-gray-500">
-                        {fileInfo.name} <br /> Tamaño:
-                        {(fileInfo.size / 1024).toFixed(1)} kB
-                      </p>
-                    </div>
-                  )}
-                  <div className="flex justify-center text-sm text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer bg-white rounded-lg bg-indigo-600 p-2 font-medium text-white hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                    >
-                      Seleccione archivo
-                    </label>
-                    <input
-                      id="file-upload"
-                      type="file"
-                      required
-                      accept="application/pdf"
-                      className="hidden"
-                      onChange={handleFileUpload}
-                    />
-                  </div>
-                  <p className="text-xs text-gray-500">PDF maximo 10MB</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button
-              type="submit"
-              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
+      <FormControl id="tipo_archivo" my={4}>
+        <FormLabel>Tipo de archivo</FormLabel>
+        <Select
+          placeholder="Seleccione uno"
+          name="tipo_archivo"
+          onChange={(e) => setFileType(e.target.value)}
+        >
+          {valTypes.map((val) => (
+            <option value={val} key={val}>
+              {val}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
+
+      <FormControl id="file-upload" my={4}>
+        <FormLabel htmlFor="file-upload">
+          Archivo(s)
+          <input
+            id="file-upload"
+            type="file"
+            multiple
+            accept="application/pdf"
+            onChange={handleFileUpload}
+            required
+            style={{ display: "none" }}
+          />
+          <Box borderColor="teal" borderWidth={2} borderRadius={8} p={2}>
+            <RenderFilesAdded files={files} />
+          </Box>
+        </FormLabel>
+        <Text fontSize="sm" color="blackAlpha.600" >Tamaño maximo de cada archivo: 10MB</Text>
+      </FormControl>
+
+      <Button type="submit">Guardar</Button>
+    </form>
   );
 }
