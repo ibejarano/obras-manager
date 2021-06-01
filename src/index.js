e8cec1be76dbc51b8ebba1235cfb7bdeddd21d05
Ignacio Bejarano <ignbejarano@gmail.com>
2021-05-31 22:50:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { setContext } from "@apollo/client/link/context";
 import { createUploadLink } from "apollo-upload-client";
 
 const uploadLink = createUploadLink({
-  uri: "http://localhost:1337/graphql",
+  uri: "https://rocky-ridge-06182.herokuapp.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
